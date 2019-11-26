40f2bf2f047c2ddedde14f5aabd05252aa320c0f
rediculus667 <rediculus667@gmail.com>
2019-11-26 18:04:22 +0500

    1

--- lab3.py
+++ lab3.py
@@ -1,30 +1,41 @@
-class  A:
+class Car:
 
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, object):
+        self.brand_name = object.get('brand')
+        self.model_name = object.get('model')
+        self.e_volume = object.get('e_volume')
+        self.transmission = object.get('transmission')
+        self.body = object.get('body')
 
-    def show(self):
-        print(self.a)
+class SUV(Car):
+    type = 'SUV'
 
-    def __repr__(self):
-        return 'class A, variable a = ' + self.a
 
-class B(A):
 
-    def show(self):
-        print(self.a + ' , this is inherited class')
 
-    def __repr__(self):
-        return 'class B(inherited), variable a = ' + self.a
 
 def main():
-    a = A('Hello')
-    b = B('Hello')
-    a.show()
-    b.show()
-    print(a.__repr__())
-    print(b.__repr__())
+    objects = [
+    {'brand':'Mazda','model':'3','e_volume':'2,0','transmission':'AT', 'body':'sedan'},
+    {'brand':'Volvo','model':'V40','e_volume':'2,0','transmission':'MT', 'body':'hatchback'},
+    {'brand':'Volkswagen','model':'Polo','e_volume':'1,6','transmission':'MT', 'body':'sedan'},
+    {'brand':'Toyota','model':'Corolla','e_volume':'1,6','transmission':'MT', 'body':'sedan'},
+    {'brand':'Citroen','model':'C4','e_volume':'1,6','transmission':'AT', 'body':'coupe'}
+    ]
 
+    suv_objects = [
+    {'brand':'Mazda','model':'3','e_volume':'2,0','transmission':'AT', 'body':'sedan'}
+    ]
 
+    car = []
+    for i in range(len(objects)):
+        car.append(Car(objects[i]))
+
+    suv = []
+    for i in range(len(suv_objects)):
+        suv.append(SUV(suv_objects[i]))
+
+    print(car[1].__repr__())
+    print(car[1].__str__())
 if __name__ == '__main__':
     main()
