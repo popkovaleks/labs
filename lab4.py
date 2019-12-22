e09483e2da1036ba85cb6b03a8962656e24f92f8
popkovaleks <55128538+popkovaleks@users.noreply.github.com>
2019-12-22 22:08:36 +0500

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,35 +1,81 @@
-class  A:
+from functools import reduce
 
-    def __init__(self, a):
-        self.a = a
 
-    def show(self):
-        print(self.a)
+class Car:
 
-    def r_show(self):
-        return self.a
+    def __init__(self, object):
+        self.brand_name = object.get('brand')
+        self.model_name = object.get('model')
+        self.e_volume = object.get('e_volume')
+        self.transmission = object.get('transmission')
 
-    def __repr__(self, show):
-        return 'class A, variable a = ' + show()
+    def test(self):
+        print(self.__repr__())
+        print(self.__str__())
 
-class B(A):
 
-    def show(self):
-        print(self.a + ' , this is inherited class')
+class SUV(Car):
+    body = 'SUV'
+    def show_info(self):
+        print('Brand name ' + self.brand_name + ', model ' + self.model_name + ', engine volume ' + self.e_volume + ', transmission ' + self.transmission + ', body ' + self.body)
+        self.test()
 
-    def r_show(self):
-        return self.a + ' , this is inherited class'
+class Coupe(Car):
+    body = 'coupe'
+    def show_info(self):
+        print('Brand name ' + self.brand_name + ', model ' + self.model_name + ', engine volume ' + self.e_volume + ', transmission ' + self.transmission + ', body ' + self.body)
+        self.test()
 
-    def __repr__(self, show):
-        return 'class B(inherited), variable a = ' + show()
+
+class Sedan(Car):
+    body = 'sedan'
+    def show_info(self):
+        print('Brand name ' + self.brand_name + ', model ' + self.model_name + ', engine volume ' + self.e_volume + ', transmission ' + self.transmission + ', body ' + self.body)
+        self.test()
+
+class Hatchback(Car):
+    body = 'hatchback'
+    def show_info(self):
+        print('Brand name ' + self.brand_name + ', model ' + self.model_name + ', engine volume ' + self.e_volume + ', transmission ' + self.transmission + ', body ' + self.body)
+        self.test()
+
+
 
 def main():
-    a = A('Hello')
-    b = B('Hello')
-    a.show()
-    b.show()
-    print(a.__repr__(a.r_show))
-    print(b.__repr__(b.r_show))
+
+
+    sedan_objects = [
+    {'brand':'Mazda','model':'3','e_volume':'2,0','transmission':'AT'},
+    {'brand':'Volkswagen','model':'Polo','e_volume':'1,6','transmission':'MT'},
+    {'brand':'Toyota','model':'Corolla','e_volume':'1,6','transmission':'MT'},
+    ]
+
+    hatchback_objects = [
+    {'brand':'Volvo','model':'V40','e_volume':'2,0','transmission':'MT'},
+    ]
+
+    suv_objects = [
+    {'brand':'Mazda','model':'CX-5','e_volume':'2,0','transmission':'AT'}
+    ]
+
+    coupe_objects = [
+    {'brand':'Citroen','model':'C4','e_volume':'1,6','transmission':'AT'}
+    ]
+
+
+    sedan = [(Sedan(sedan_objects[i])) for i in range(len(sedan_objects))]
+    hatchback = [(Hatchback(hatchback_objects[i])) for i in range(len(hatchback_objects))]
+    suv = list(map(SUV, suv_objects))
+    coupe = list(map(Coupe, coupe_objects))
+
+
+    counts_objects = [len(sedan_objects), len(hatchback_objects), len(suv_objects), len(coupe_objects)]
+    sum_objects = reduce(lambda a, b: a + b, counts_objects)
+    print(sum_objects)
+
+
+    
+    suv[0].show_info()
 
 
 if __name__ == '__main__':
